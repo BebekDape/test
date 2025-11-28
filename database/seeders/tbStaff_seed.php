7812be930e05af4e918fbdf31e5348f7172ea91d
BebekDape <543241079@smktelkom-bjb.sch.id>
2025-11-28 18:00:47 +0800

    Readme changes

---
+++ database/seeders/tbStaff_seed.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class tbStaff_seed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', ('welcome'));
-Route::view('/home-page', ('home'));
+Route::view('/', 'welcome');
+Route::view('/home-page', 'home');
